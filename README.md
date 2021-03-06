1b0f02a421a8f0d4dd0baa3dc81821b6a7a0344b
islishude <islishude@gmail.com>
2021-03-06 21:46:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+OxDex Router
---
+++ test/shared/fixtures.ts
@@ -0,0 +1,70 @@
+import { Wallet, Contract } from 'ethers'
+import { Web3Provider } from 'ethers/providers'
+import { deployContract } from 'ethereum-waffle'
+
+import { expandTo18Decimals } from './utilities'
+
+import OxDexFactory from 'oxdex-contract/build/OxDexFactory.json'
+import IOxDexPair from 'oxdex-contract/build/IOxDexPair.json'
+
+import ERC20 from '../../build/ERC20.json'
+import WETH9 from '../../build/WETH9.json'
+import OxDexRouter from '../../build/OxDexRouter.json'
+import RouterEventEmitter from '../../build/RouterEventEmitter.json'
+
+const overrides = {
+    gasLimit: 9999999
+}
+
+interface Fixture {
+    token0: Contract
+    token1: Contract
+    WETH: Contract
+    WETHPartner: Contract
+    factory: Contract
+    routerEventEmitter: Contract
+    router: Contract
+    pair: Contract
+    WETHPair: Contract
+}
+
+export async function v2Fixture(provider: Web3Provider, [wallet]: Wallet[]): Promise<Fixture> {
+    // deploy tokens
+    const tokenA = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+    const tokenB = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+    const WETH = await deployContract(wallet, WETH9)
+    const WETHPartner = await deployContract(wallet, ERC20, [expandTo18Decimals(10000)])
+
+    const factory = await deployContract(wallet, OxDexFactory, [wallet.address])
+
+    // deploy routers
+    const router = await deployContract(wallet, OxDexRouter, [factory.address, WETH.address], overrides)
+
+    // event emitter for testing
+    const routerEventEmitter = await deployContract(wallet, RouterEventEmitter, [])
+
+    // initialize
+    await factory.createPair(tokenA.address, tokenB.address)
+    const pairAddress = await factory.getPair(tokenA.address, tokenB.address)
+    const pair = new Contract(pairAddress, JSON.stringify(IOxDexPair.abi), provider).connect(wallet)
+
+    const token0Address = await pair.token0()
+    const token0 = tokenA.address === token0Address ? tokenA : tokenB
+    const token1 = tokenA.address === token0Address ? tokenB : tokenA
+
+    await factory.createPair(WETH.address, WETHPartner.address)
+    const WETHPairAddress = await factory.getPair(WETH.address, WETHPartner.address)
+    const WETHPair = new Contract(WETHPairAddress, JSON.stringify(IOxDexPair.abi), provider).connect(wallet)
+
+    return {
+        token0,
+        token1,
+        WETH,
+        WETHPartner,
+        factory: factory,
+        router: router, // the default router, 01 had a minor bug
+        routerEventEmitter,
+        pair,
+        WETHPair
+    }
+}
